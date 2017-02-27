233b775a7cd1d6e0c337f8ad2692268d2e922888
Abi Peterson <abipeterson07@gmail.com>
2017-02-27 23:46:01 +0000

    did the things

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+var c2cEducators = ["Matt", "Trey", "Sam", "Samuel"];
+var c2cStudents = ["Adrian", "Richard", "Tim", "Jorge", "Garrett", "Evila", "Eyesac", "Alexis", "Corey", "Basil", "Kaci", "Spencer"];
 
-
-
+$("#studentButton").click(function() {
+    $("#studentDisplay").append(c2cStudents[Math.floor(Math.random()*(c2cStudents.length))] + ", please answer the question."); 
+});
+$("#teacherButton").click(function() {
+    $("#teacherDisplay").append(c2cEducators[Math.floor(Math.random()*(c2cEducators.length))] + ", please help :("); 
+});
 });
