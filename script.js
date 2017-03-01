060cd115704be73222e87d99e3c4eb8d8df1b92b
Abi Peterson <abipeterson07@gmail.com>
2017-03-01 23:23:00 +0000

    finished everything

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ var c2cEducators = ["Matt", "Trey", "Sam", "Samuel"];
 var c2cStudents = ["Adrian", "Richard", "Tim", "Jorge", "Garrett", "Evila", "Eyesac", "Alexis", "Corey", "Basil", "Kaci", "Spencer"];
 
 $("#studentButton").click(function() {
-    $("#studentDisplay").append(c2cStudents[Math.floor(Math.random()*(c2cStudents.length))] + ", please answer the question."); 
+    $("#studentDisplay").text(c2cStudents[Math.floor(Math.random()*(c2cStudents.length))] + ", please answer the question."); 
 });
 $("#teacherButton").click(function() {
-    $("#teacherDisplay").append(c2cEducators[Math.floor(Math.random()*(c2cEducators.length))] + ", please help :("); 
+    $("#teacherDisplay").text(c2cEducators[Math.floor(Math.random()*(c2cEducators.length))] + ", please help :("); 
+});
+$("#add").click(function() {
+   var newStudent = $("#student").val();
+   c2cStudents.push(newStudent);
+   if(newStudent.length() > 1){
+   $("#addedDisplay").text(newStudent + " has been added to your class.");
+   }
+});
+$("#addT").click(function() {
+   var newTeacher = $("#teacher").val();
+   c2cEducators.push(newTeacher);
+   if(newTeacher.length() > 1){
+   $("#addedTeachDisplay").text(newTeacher + " has been added as a teacher.");
+   }
 });
 });
